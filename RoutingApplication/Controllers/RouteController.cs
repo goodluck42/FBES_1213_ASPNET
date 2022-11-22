2ffa8cc10bf423cbeee5cfbd6b3a3bfb399c37b0
Alex <xrlogic1@gmail.com>
2022-11-22 20:15:50 +0400

    Lesson 8

---
+++ RoutingApplication/Controllers/RouteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingApplication.Controllers
+{
+	// localhost:1234/route/main
+	[Route("user")]
+	// [Route("/user/{action=main}/{id:int?}")]
+	public class RouteController : Controller
+	{
+		[Route("main")]
+		public string Index()
+		{
+			return $"user/main";
+		}
+
+		[Route("add/{name?}/{age:int?}")]
+		public string Create(string? name, int? age)
+		{
+			return $"user/add name={name} age={age}";
+		}
+		
+		[Route("get/{id:int?}")]
+		public string Get(int? id)
+		{
+			return $"user/get id={id}";
+		}
+	}
+}
---
+++ RoutingApplication/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Routing.Constraints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}",
+	constraints: new {
+		id = new IntRouteConstraint()
+	});
+
+//app.MapControllerRoute(
+//	name: "default",
+//	pattern: "Route/{action=Index}/{id?}");
+
+app.Run();
